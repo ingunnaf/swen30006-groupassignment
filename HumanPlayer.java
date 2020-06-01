b93b08c6b64b2a69c9207f91bf8698f54cbbaad1
Ingunn <61862589+iflovig@users.noreply.github.com>
2020-06-01 20:51:30 +0200

    Update HumanPlayer.java

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -3,14 +3,14 @@ import ch.aplu.jcardgame.*;
 public class HumanPlayer extends AbstractPlayer {
 
     public HumanPlayer(Hand h1) {
-        super(h1);
+        this.hand = h1;
         CardListener cardListener = new CardAdapter();  // Human Player plays card
         super.hand.addCardListener(cardListener);
     }
 
-    public Card returnMove(Card selected) {
-        /** */
-        return selected;
+    public Card returnMove(Hand hand) {
+        /** this isn't a working function but is just here to comply with abstractplayer outline*/
+        return hand.getCard(0); // just a placeholder
     }
 
     //public void leftDoubleClicked(Card card) { selected = card; hands[0].setTouchEnabled(false); }
